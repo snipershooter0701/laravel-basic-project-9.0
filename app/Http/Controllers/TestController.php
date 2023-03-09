5e980682d3fe5a6dff6dc63979b77a78bee59ec6
snipershooter <snipershooter0701@gmail.com>
2023-03-09 09:05:13 +0200

    laravel-basic-project-9.0

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Test;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = Test::latest()->paginate(5);
+        return view('index', compact('data'))->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name'          =>  'required',
+            'email'         =>  'required|email|unique:students',
+            'image'         =>  'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048|dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000'
+        ]);
+
+        $file_name = time() . '.' . request()->image->getClientOriginalExtension();
+
+        request()->image->move(public_path('images'), $file_name);
+
+        $student = new Student;
+
+        $student->name = $request->name;
+        $student->email = $request->email;
+        $student->gender = $request->gender;
+        $student->image = $file_name;
+
+        $student->save();
+
+        return redirect()->route('students.index')->with('success', 'Student Added successfully.');
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Test $test)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Test $test)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Test  $test
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Test $test)
+    {
+        //
+    }
+}
